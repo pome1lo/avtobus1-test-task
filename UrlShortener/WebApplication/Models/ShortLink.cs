58344d1d272fa9a6085b458adf5f3b6b3e16f876
Alexey <paaworker@gmail.com>
2025-02-11 17:34:40 +0300

    Created the ShortLink model

---
+++ UrlShortener/WebApplication/Models/ShortLink.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication.Models
+{
+    public class ShortLink
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; } = string.Empty;
+        public string ShortUrl { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int ClickCount { get; set; } = 0;
+    }
+}
