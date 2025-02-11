c80a518505d9e325554e78c1c7f36c0c908ca495
Alexey <paaworker@gmail.com>
2025-02-11 20:03:16 +0300

    Created the ShortUrlGenerator class / Created the IShortLinkService interface

---
+++ UrlShortener/WebApplication/Services/Interfaces/IShortLinkService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication.Models;
+
+namespace WebApplication.Services.Interfaces
+{
+    public interface IShortLinkService
+    {
+        Task<ShortLink> CreateShortLinkAsync(string originalUrl);
+        Task<ShortLink> GetShortLinkByShortUrlAsync(string shortUrl);
+        Task<IEnumerable<ShortLink>> GetAllShortLinksAsync();
+        Task IncrementClickCountAsync(string shortUrl);
+        Task DeleteShortLinkAsync(int id);
+    }
+}
---
+++ UrlShortener/WebApplication/Utils/ShortUrlGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication.Utils
+{
+    internal static class ShortUrlGenerator
+    {
+        internal static string GenerateShortUrl()
+        {
+            return Guid.NewGuid().ToString("N").Substring(0, 8);
+        }
+    }
+}
