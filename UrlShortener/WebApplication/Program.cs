3bb2ddb33603c4fabea9dcde9d7f051e2e791b16
Alexey <95231870+pome1lo@users.noreply.github.com>
2025-02-14 16:22:25 +0300

    Update Program.cs

--- UrlShortener/WebApplication/Program.cs
+++ UrlShortener/WebApplication/Program.cs
@@ -23,6 +23,12 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.UseGlobalExceptionHandler();
 
 app.UseStaticFiles();
